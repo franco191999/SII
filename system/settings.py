5ed35984399f9de922f25668b6e4e4a12dca87bb
Franco León <franco19.1999@gmail.com>
2022-12-06 16:52:33 -0500

    db

--- system/settings.py
+++ system/settings.py
@@ -110,12 +110,8 @@ WSGI_APPLICATION = 'system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sii',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
